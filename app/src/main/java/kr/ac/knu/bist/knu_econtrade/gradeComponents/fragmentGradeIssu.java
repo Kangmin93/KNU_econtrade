a49189707478957eb1eb5525c01f872c1aa1e02b
dykim <ejrdbs23@naver.com>
2016-12-17 02:31:44 +0900

    .

--- app/src/main/java/kr/ac/knu/bist/knu_econtrade/gradeComponents/fragmentGradeIssu.java
+++ app/src/main/java/kr/ac/knu/bist/knu_econtrade/gradeComponents/fragmentGradeIssu.java
@@ -130,17 +130,17 @@ public class fragmentGradeIssu extends Fragment {
 
         if (activeNetwork != null) { // connected to the internet
             if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) { //와이파이 여부
-                return true;
+                return false;
             }
             else if (activeNetwork.getType() == ConnectivityManager.TYPE_WIFI) { //모바일 데이터 여부
-                return true;
+                return false;
             }
             else if (activeNetwork.getType() == ConnectivityManager.TYPE_ETHERNET)
-                return true;
+                return false;
         } else {
             // not connected to the internet
         }
-        return false;
+        return true;
     }
 
     private void processRetrieveItems() throws IOException {
