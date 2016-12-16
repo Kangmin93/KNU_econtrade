c52acfdb337ae9a96626d6cf1d8c2335b4c60cd3
liliilli <vertx.ngng@gmail.com>
2016-12-16 11:08:08 +0900

    manifest 갱신

--- app/src/main/java/kr/ac/knu/bist/knu_econtrade/Activities/Main_Council_MainScene.java
+++ app/src/main/java/kr/ac/knu/bist/knu_econtrade/Activities/Main_Council_MainScene.java
@@ -1,7 +1,9 @@
 package kr.ac.knu.bist.knu_econtrade.Activities;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Vertx on 2016-09-10.
  */
-public class Main_Council_MainScene {
+public class Main_Council_MainScene extends AppCompatActivity {
 }
