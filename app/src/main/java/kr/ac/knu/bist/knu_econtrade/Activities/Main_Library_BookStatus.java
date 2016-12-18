9fa9bc42af5314f24e7390ee4a4def7e749c6f67
dykim <ejrdbs23@naver.com>
2016-12-18 16:41:14 +0900

    도서 검색 레이아웃 - 2

--- app/src/main/java/kr/ac/knu/bist/knu_econtrade/Activities/Main_Library_BookStatus.java
+++ app/src/main/java/kr/ac/knu/bist/knu_econtrade/Activities/Main_Library_BookStatus.java
@@ -2,6 +2,9 @@ package kr.ac.knu.bist.knu_econtrade.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 import kr.ac.knu.bist.knu_econtrade.R;
 
@@ -10,8 +13,20 @@ import kr.ac.knu.bist.knu_econtrade.R;
  */
 public class Main_Library_BookStatus extends AppCompatActivity{
 
+    private EditText book_search_name;
+    private Button book_search_button;
+    private ListView book_list;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activity_library_book);
+
+        book_search_button = (Button)findViewById(R.id.library_search);
+        book_search_name=(EditText)findViewById(R.id.book_search_name);
+        book_list = (ListView)findViewById(R.id.listView);
+
+        /*                                                  선언                              */
+
+
+
     }
 }
