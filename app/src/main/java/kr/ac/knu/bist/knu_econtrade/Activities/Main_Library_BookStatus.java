efbcd015be6a59607276fc2e9dfbd95671ed60cb
dykim <ejrdbs23@naver.com>
2016-12-18 16:55:38 +0900

    도서 검색 기본 레이아웃 작업 해놓음.

--- app/src/main/java/kr/ac/knu/bist/knu_econtrade/Activities/Main_Library_BookStatus.java
+++ app/src/main/java/kr/ac/knu/bist/knu_econtrade/Activities/Main_Library_BookStatus.java
@@ -2,9 +2,11 @@ package kr.ac.knu.bist.knu_econtrade.Activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import kr.ac.knu.bist.knu_econtrade.R;
 
@@ -16,6 +18,7 @@ public class Main_Library_BookStatus extends AppCompatActivity{
     private EditText book_search_name;
     private Button book_search_button;
     private ListView book_list;
+    private String book_search_name_get;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activity_library_book);
@@ -25,7 +28,26 @@ public class Main_Library_BookStatus extends AppCompatActivity{
         book_list = (ListView)findViewById(R.id.listView);
 
         /*                                                  선언                              */
+        book_search_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                book_search_name_get = book_search_name.getText().toString();
+                if(book_search_name_get.equals("")){
+                    Toast.makeText(getApplicationContext(),"도서 이름을 입력하세요",Toast.LENGTH_SHORT).show();
+                }else{
+
+                }
+
+            }
+        });
+
+
+    }
+
+    private void bookSearch(String book_name) {
+
+        //학교 도서관 홈페이지에 book name을 검색 인자로 request를 날려 결과를 받아오고 listview에 뿌려준다.
 
 
     }
